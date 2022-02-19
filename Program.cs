6fb3193c39123fad0768e3ad998896f32d28ce93
Gökhan Çam <76878009+gokcam@users.noreply.github.com>
2022-02-19 12:38:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cuma);
+
+            int sicaklik = 32;
+            if(sicaklik<=(int)HavaDurumu.ÇokSıcak)
+            {
+                Console.WriteLine("Hava çok Sıcak");
+            }
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Sali,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+
+    }
+    enum HavaDurumu
+    {
+        Soğuk =5,
+        Normal = 20,
+        Sıcak =30,
+        ÇokSıcak = 40
+    }
+}
